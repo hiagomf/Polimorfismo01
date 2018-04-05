ebcdc247832b9728ad96a4e845f25541c4dbe8e7
Hiago <Hiago@DESKTOP-LF9IFC6>
2018-04-05 19:21:01 -0300

    Multiplicação e Divisão

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,21 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-/**
- *
- * @author Hiago
- */
+import absctract.OperacaoMatematica;
+import java.util.Scanner;
+import operacoes.Soma;
+import operacoes.Subtracao;
+import operacoes.Divisao;
+import operacoes.Multiplicacao;
+
+
 public class Start {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    //OperacaoMatematica op define a operação que vai ser usada.
+    public static void mostrarCalculo(OperacaoMatematica op, double x, double y) {
+        System.out.println("Resultado da operação: " + op.calcular(x, y));
+    }
+
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        System.out.println("Informe dois valores: ");
+        double x = e.nextDouble();
+        double y = e.nextDouble();
+        
+        //Instancia new Soma(), em op para passar o objeto instanciado ficado : new Soma().calcular(x,y) lá no método mostrarCalculo.
+        System.out.println("A soma é: ");
+        mostrarCalculo(new Soma(), x, y);
+        
+        //Instancia new Subtracao(), em op para passar o objeto instanciado ficado : new Soma().calcular(x,y) lá no método mostrarCalculo.
+        System.out.println("A subtração é: ");
+        mostrarCalculo(new Subtracao(), x, y);
+        
+        System.out.println("A multiplicação é: ");
+        mostrarCalculo(new Multiplicacao(), x, y);
+        
+        System.out.println("A divisão é: ");
+        mostrarCalculo(new Divisao(), x, y);
+    }
+
 }
